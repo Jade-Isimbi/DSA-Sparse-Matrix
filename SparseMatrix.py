07c9df100529bfc0f58d1665cf3cdd6acf65ef2c
Jade-Isimbi <156232169+Jade-Isimbi@users.noreply.github.com>
2024-10-06 11:27:17 +0200

    Corrected the path to the input files

--- SparseMatrix.py
+++ SparseMatrix.py
@@ -132,8 +132,10 @@ class SparseMatrix:
 def main():
     try:
         # Load matrices from files
-        matrix1 = SparseMatrix.load_from_file("/home/jade/sparse_matrix/sample_inputs/easy_sample_02_1.txt")
-        matrix2 = SparseMatrix.load_from_file("/home/jade/sparse_matrix/sample_inputs/easy_sample_02_2.txt")
+        matrix1 = SparseMatrix.load_from_file("/home/jade/DSA-Sparse-Matrix
+/sample_inputs/easy_sample_02_1.txt")
+        matrix2 = SparseMatrix.load_from_file("/home/jade/DSA-Sparse-Matrix
+/sample_inputs/easy_sample_02_2.txt")
 
         # Get user input for operation
         print("\nChoose an operation:")
