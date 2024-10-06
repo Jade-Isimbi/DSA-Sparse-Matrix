d6e879a386d7fc82c7caa0f11b0874b02ba2e136
Jade-Isimbi <156232169+Jade-Isimbi@users.noreply.github.com>
2024-10-06 11:36:17 +0200

    Update SparseMatrix.py

--- SparseMatrix.py
+++ SparseMatrix.py
@@ -132,8 +132,8 @@ class SparseMatrix:
 def main():
     try:
         # Load matrices from files
-        matrix1 = SparseMatrix.load_from_file("/home/jade/DSA-Sparse-Matrix/sample_inputs/easy_sample_02_2.txt")
-        matrix2 = SparseMatrix.load_from_file("/home/jade/DSA-Sparse-Matrix/sample_inputs/easy_sample_02_2.txt")
+        matrix1 = SparseMatrix.load_from_file("/home/jade/DSA-Sparse-Matrix/sample_inputs/easy_sample_01_2.txt")
+        matrix2 = SparseMatrix.load_from_file("/home/jade/DSA-Sparse-Matrix/sample_inputs/easy_sample_01_2.txt")
 
         # Get user input for operation
         print("\nChoose an operation:")
